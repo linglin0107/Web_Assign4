609807a2c3c8f8cd64918df451afcacfa38f0066
linglin0107 <37716364+linglin0107@users.noreply.github.com>
2018-11-28 20:31:19 -0500

    Add files via upload

---
+++ product.js
@@ -0,0 +1,12 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+var ProductSchema   = new Schema({
+    _id:String,
+    title: String,
+    price: Number,
+    instock : Boolean,
+    photo : String ,
+});
+module.exports = mongoose.model('Product', ProductSchema);
+// module.exports = mongoose.model('Product', ProductSchema,'optiponally pass schema name ');
+
